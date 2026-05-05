edb7fcf05e3e7bc4a0a4f0e0bd51fb013303ae6d
239 <239@pm.me>
2026-05-05 23:10:17 +0200

    Set executable names before build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,11 @@ application {
 
 // https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html
 graalvmNative {
+    val type = System.getProperty("os.name").lowercase().substringBefore(' ')
+    val arch = System.getProperty("os.arch").lowercase().substringBefore(' ')
     binaries {
         named("main") {
-            imageName.set(project.name)
+            imageName.set("${project.name}-$type-$arch")
             buildArgs.add("--enable-native-access=ALL-UNNAMED")
         }
     }

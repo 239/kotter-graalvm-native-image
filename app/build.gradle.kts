36a063f05be8e49dc64dcc3e45e2daf36765b2b9
239 <239@pm.me>
2026-05-05 21:33:31 +0200

    Add example files

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    application
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.graalvm.native)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://central.sonatype.com/repository/maven-snapshots/") { //TODO !
+        mavenContent {
+            includeGroup("com.varabyte.kotter")
+            snapshotsOnly()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.kotter.jvm)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass = "org.example.AppKt"
+}
+
+// https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html
+graalvmNative {
+    binaries {
+        named("main") {
+            buildArgs.add("--enable-native-access=ALL-UNNAMED")
+        }
+    }
+}

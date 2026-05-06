d71129a0bfe679b9e1fc10cb582f68b1d9d711ed
239 <239@pm.me>
2026-05-06 23:01:10 +0200

    Make demo more interesting

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,25 +1,42 @@
 package org.example
 
 import com.varabyte.kotter.foundation.*
-import com.varabyte.kotter.foundation.collections.*
 import com.varabyte.kotter.foundation.input.*
 import com.varabyte.kotter.foundation.text.*
+import com.varabyte.kotter.foundation.timer.*
 import com.varabyte.kotter.terminal.system.*
 
+const val chars = "♔♕♖♗♘♙♚♛♜♝♞♟"
+
 fun main() = session(SystemTerminal()) { //excluding VirtualTerminal from native image
-    val keys = liveListOf<String>()
+    val list = mutableListOf<BC>()
+    val duration = kotlin.time.Duration.parse("10ms")
+    var column = 0
+    var cycle by liveVarOf(0)
     section {
-        text("Press any key (and "); bold { text('Q') }; textLine(" will quit)")
-        keys.distinct().run {
-            textLine("Distinct keys pressed: $size [${joinToString(" ")}]")
+        BC.width = width
+        BC.height = height
+        for (row in 0..height) {
+            val line = CharArray(width) { ' ' }
+            list.filter { it.py.toInt() == row }.forEach {
+                if (it.px.toInt() in line.indices)
+                    line[it.px.toInt()] = it.char
+            }
+            textLine(String(line)) //TODO colors?
         }
-        textLine("All keys pressed: ${keys.size} [${keys.joinToString(" ")}]")
-    }.runUntilKeyPressed(Keys.Q) {
+        invert()
+        text("$cycle•${list.size}:${list.joinToString("") { "${it.char}" }}"
+            .take(width).let { it + " ".repeat(width - it.length) })
+    }.runUntilKeyPressed(Keys.Escape) {
         onKeyPressed {
-            when (key) {
-                Keys.Space -> "Space"
-                else       -> "$key"
-            }.let { keys.add(it) }
+            if (key == Keys.Space) list.add(BC(chars.random(), column++.toDouble()))
+            else "$key".forEach { list.add(BC(it, column++.toDouble())) }
+            column = column.mod(BC.width)
+        }
+        addTimer(duration, true) {
+            if (list.isNotEmpty()) cycle++
+            list.removeIf { it.bounces > 33 }
+            list.forEach { it.update() }
         }
     }
 }
---
+++ app/src/main/kotlin/org/example/BC.kt
@@ -0,0 +1,41 @@
+package org.example
+
+import kotlin.random.*
+
+class BC(
+    val char: Char,
+    var px: Double = 0.0,
+    var py: Double = 0.0,
+    var bounces: Int = 0
+) {
+    private val restitution = Random.nextDouble(0.6, 0.9)
+    private var vx = Random.nextDouble(-drift, drift)
+    private var vy = 0.0
+
+    companion object {
+        var width = 1
+        var height = 1
+        var gravity = 0.1
+        var drift = 0.9
+    }
+
+    fun update() {
+        vy += gravity
+        px += vx
+        py += vy
+        if (px < 0) {
+            px = 0.0
+            vx *= -restitution
+        }
+        if (px > width) {
+            px = width.toDouble()
+            vx *= -restitution
+        }
+        if (py > height) {
+            py = height.toDouble()
+            vy *= -restitution
+            vx *= 0.9
+            bounces++
+        }
+    }
+}

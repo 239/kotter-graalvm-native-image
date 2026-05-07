59a6163e6b50ae260547d59db89ee0796f6c0276
239 <239@pm.me>
2026-05-07 15:51:55 +0200

    Add welcome message and colors

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -5,38 +5,48 @@ import com.varabyte.kotter.foundation.input.*
 import com.varabyte.kotter.foundation.text.*
 import com.varabyte.kotter.foundation.timer.*
 import com.varabyte.kotter.terminal.system.*
+import kotlin.time.*
 
 const val chars = "♔♕♖♗♘♙♚♛♜♝♞♟"
+const val hello = "Hello, press any key to continue! (Esc to exit)"
 
+//fun main() = session { //while developing
 fun main() = session(SystemTerminal()) { //excluding VirtualTerminal from native image
     val list = mutableListOf<BC>()
-    val duration = kotlin.time.Duration.parse("10ms")
-    var column = 0
+    val duration = 10.toDuration(DurationUnit.MILLISECONDS)
+    var column = hello.length
+    var hue = 180
     var cycle by liveVarOf(0)
+    hello.forEachIndexed { i, c -> list.add(BC(c, i.toDouble(), 2.0)) }
     section {
         BC.width = width
         BC.height = height
+        hue = (hue + 1) % 360
+        hsv(hue, 1.0f, 1.0f)
         for (row in 0..height) {
-            val line = CharArray(width) { ' ' }
+            val line = CharArray(width) { ' ' } //TODO reuse?
             list.filter { it.py.toInt() == row }.forEach {
                 if (it.px.toInt() in line.indices)
                     line[it.px.toInt()] = it.char
             }
-            textLine(String(line)) //TODO colors?
+            textLine(String(line))
         }
         invert()
-        text("$cycle•${list.size}:${list.joinToString("") { "${it.char}" }}"
+        text("${list.size}•${list.joinToString("") { "${it.char}" }}"
             .take(width).let { it + " ".repeat(width - it.length) })
     }.runUntilKeyPressed(Keys.Escape) {
         onKeyPressed {
-            if (key == Keys.Space) list.add(BC(chars.random(), column++.toDouble()))
+            if (key == Keys.Space) list.add(BC(chars.random(), column++.toDouble(), 1.0))
             else "$key".forEach { list.add(BC(it, column++.toDouble())) }
-            column = column.mod(BC.width)
+            column %= BC.width
+            if (cycle < 1) cycle = 1 //kick off
         }
         addTimer(duration, true) {
-            if (list.isNotEmpty()) cycle++
-            list.removeIf { it.bounces > 33 }
-            list.forEach { it.update() }
+            if (cycle > 0) {
+                if (list.isNotEmpty()) cycle++
+                list.removeIf { it.bounces > 39 }
+                list.forEach { it.update() }
+            }
         }
     }
 }

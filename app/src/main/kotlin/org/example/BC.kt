ae6648657968ad42e9f84b3f2bf5d505fd17a4e8
239 <239@pm.me>
2026-05-07 15:28:42 +0200

    Reduce drift

--- app/src/main/kotlin/org/example/BC.kt
+++ app/src/main/kotlin/org/example/BC.kt
@@ -6,17 +6,17 @@ class BC(
     val char: Char,
     var px: Double = 0.0,
     var py: Double = 0.0,
-    var bounces: Int = 0
 ) {
     private val restitution = Random.nextDouble(0.6, 0.9)
     private var vx = Random.nextDouble(-drift, drift)
     private var vy = 0.0
+    var bounces: Int = 0
 
     companion object {
         var width = 1
         var height = 1
         var gravity = 0.1
-        var drift = 0.9
+        var drift = 0.6
     }
 
     fun update() {

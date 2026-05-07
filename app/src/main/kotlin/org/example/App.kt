b846721ebcdc0828ec0ffef6ec27254bc219144f
239 <239@pm.me>
2026-05-07 17:30:56 +0200

    Apply new boundaries

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -10,23 +10,23 @@ import kotlin.time.*
 const val chars = "♔♕♖♗♘♙♚♛♜♝♞♟"
 const val hello = "Hello, press any key to continue! (Esc to exit)"
 
-//fun main() = session { //while developing
-fun main() = session(SystemTerminal()) { //excluding VirtualTerminal from native image
+fun main() = session { //while developing
+//fun main() = session(SystemTerminal()) { //excluding VirtualTerminal from native image
     val list = mutableListOf<BC>()
     val duration = 10.toDuration(DurationUnit.MILLISECONDS)
     var column = hello.length
     var hue = 180
     var cycle by liveVarOf(0)
-    hello.forEachIndexed { i, c -> list.add(BC(c, i.toDouble(), 2.0)) }
+    hello.forEachIndexed { i, c -> list.add(BC(c, i.toDouble(), 1.0)) }
     section {
-        BC.width = width
-        BC.height = height
+        BC.limitx = width - 1.0
+        BC.limity = height - 1.0
         hue = (hue + 1) % 360
         hsv(hue, 1.0f, 1.0f)
-        for (row in 0..height) {
+        for (row in 1 until height) {
             val line = CharArray(width) { ' ' } //TODO reuse?
             list.filter { it.py.toInt() == row }.forEach {
-                if (it.px.toInt() in line.indices)
+                if (it.px.toInt() in line.indices) //window resizing!
                     line[it.px.toInt()] = it.char
             }
             textLine(String(line))
@@ -37,8 +37,8 @@ fun main() = session(SystemTerminal()) { //excluding VirtualTerminal from native
     }.runUntilKeyPressed(Keys.Escape) {
         onKeyPressed {
             if (key == Keys.Space) list.add(BC(chars.random(), column++.toDouble(), 1.0))
-            else "$key".forEach { list.add(BC(it, column++.toDouble())) }
-            column %= BC.width
+            else "$key".forEach { list.add(BC(it, column++.toDouble(), 1.0)) }
+            column %= BC.limitx.toInt()
             if (cycle < 1) cycle = 1 //kick off
         }
         addTimer(duration, true) {
